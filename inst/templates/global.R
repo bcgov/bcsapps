8aef2c279cf8a90fabd83dd76503058fac726146
stephanieyurchak <Stephanie.Yurchak@gov.bc.ca>
2021-12-01 09:08:23 -0800

    adding packages to load

--- inst/templates/global.R
+++ inst/templates/global.R
@@ -1,4 +1,17 @@
 
+## Some useful libraries
+library(shiny)
+library(shinydashboard)
+library(tidyverse)  ## for data manipulation
+library(janitor)    ## for cleaning data (includes rounding functions)
+library(lubridate)  ## for dates
+library(ggplot2)    ## for plots
+library(plotly)     ## for interactive plots
+library(DT)         ## for tables
+
+
 ## Add code that you want to run before your app launches
 
 ### e.g., reading in data
+
+
