bf4362b1df7218593430211000a59c09efaf9b93
Yurchak <Stephanie.Yurchak@gov.bc.ca>
2025-05-15 10:02:30 -0700

    add Tim's is_local code for filepath handling

--- inst/templates/global.R
+++ inst/templates/global.R
@@ -22,9 +22,15 @@ library(ggplot2)    ## for plots
 library(plotly)     ## for interactive plots
 library(DT)         ## for tables
 
+## boolean for whether running global.R directly or if running via app.R
+## this is really helpful in development for file paths as the root folder
+## differs depending on whether the code is run via app.R or directly
+is_local = Sys.getenv('SHINY_PORT') == ""
+
 
 ## Add code that you want to run before your app launches
 
 ### e.g., reading in data
-
+## use is_local in file paths as exampled below
+## data = readRDS(here::here(ifelse(is_local, 'app', '.'), "data.Rds"))
 
